1fc2ce5a6f0f9e74fa5ca57f4900d6f583728054
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-08-27 16:40:22 +0800

    WIP

--- DotStar/DotStar.py
+++ DotStar/DotStar.py
@@ -9,12 +9,19 @@ import random
 import tempfile
 import zipfile
 import logging
+import json
+import shutil
+import platform
+import subprocess
+from distutils.version import StrictVersion
 
 # INFO
 __version__ = "0.1"
 
 # CONSTANTS
 PACKAGE_INFO_FILE = "Package.json"
+CURRENT_PLATFORM = sys.platform
+CURRENT_VERSION = StrictVersion(__version__)
 
 # VARIABLES
 
@@ -24,6 +31,9 @@ def print_help():
     Prints some help information to the console screen
     """
     print("DotStar version " + __version__)
+    print("Command-line arguments: ")
+    print("-h\t--help\tShow this help page")
+    print("-n\t--no-gui\tStart without a GUI")
 
 
 def open_file(file_path):
@@ -37,9 +47,45 @@ def open_file(file_path):
 
         # Process file
         package_info_file = os.path.join(temp_dir, PACKAGE_INFO_FILE)
+        try:
+            with open(package_info_file) as package_info_json:
+                data = json.load(package_info_json)
+
+                # Check the "DotStar Information area"
+                version_used_to_compile = StrictVersion(data["DotStar Information"]["Version"])
+                if CURRENT_VERSION < version_used_to_compile:
+                    pass
+
+                # Check the type
+                if "Application Information" in data:
+                    info = data["Application Information"]
+                    resources = info["Resources"]
+                    for resource in resources:
+                        if resource == "html":
+                            print("hey")
+                        if resource == "executable-win32":
+                            if CURRENT_PLATFORM == "win32":
+                                path_to_exe = os.path.join(temp_dir, resources[resource])
+                                subprocess.run(path_to_exe)
+                        if resource == "executable-win64":
+                            pass
+                        if resource == "executable":
+                            pass
+                elif "Document Information" in data:
+                    info = data["Document Information"]
+                    resources = info["Resources"]
+                    for resource in resources:
+                        pass
+                else:
+                    # Empty file
+                    pass
+        except FileNotFoundError:
+            raise FileNotFoundError
+        except json.JSONDecodeError:
+            logging.critical("Error decoding JSON")
 
         # If necessary, clean up the temporary directory
-        
+        shutil.rmtree(temp_dir)
     except zipfile.BadZipFile:
         logging.critical("Bad zip file!")
     except FileNotFoundError:
@@ -79,5 +125,7 @@ if __name__ == "__main__":
     for argument in args:
         if argument == "-h" or argument == "--help":
             print_help()
+        if argument == "-n" or argument == "--no-gui":
+            pass
         else:
             open_file(argument)

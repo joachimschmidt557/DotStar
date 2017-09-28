42beff74924488de4f8452d15c5db6f4d9baf053
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-09-28 22:14:49 +0800

    Testing...

--- DotStar/Package.py
+++ DotStar/Package.py
@@ -35,6 +35,7 @@ def compile_package():
     Compile DotStar
     """
     print("Compiling DotStar...")
+    os.system("pyinstaller DotStar\\DotStar.py")
     os.system("%PYTHON%\\python.exe -m pyinstaller DotStar\\DotStar.py")
     print("Creating Windows binaries")
     os.system("%PYTHON%\\python.exe -m py2exe.build_exe DotStar\\DotStar.py")

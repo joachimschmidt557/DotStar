2c5df469793d1c0fdae16ec65bb20d1ff6650375
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-10-15 22:16:19 +0800

    Add platform-specific Package scripts

---
+++ DotStar/Package.Win.Install.bat
@@ -0,0 +1,4 @@
+@echo off
+rem This script will install DotStar on the machine
+
+copy .\dist\DotStar.exe %LOCALAPPDATA%\DotStar\ /Y
---
+++ DotStar/Package.Win.Uninstall.bat
@@ -0,0 +1,4 @@
+@echo off
+rem This script will uninstall DotStar on the machine
+
+rmdir %LOCALAPPDATA%\DotStar\ /S /Q

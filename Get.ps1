bbac60a79a44b027536f7019a3521b459b4a4c65
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-11-07 12:47:54 +0800

    WIP

--- Get.ps1
+++ Get.ps1
@@ -1,3 +1,12 @@
+Write-Host Quick Dotstar Installer
+
+Write-Host Downloading DotStar...
 $url = "https://github.com/joachimschmidt557/DotStar/releases/download/v0.1.1-alpha/dotstar.exe"
 $destination = Join-Path (Join-Path "$Env:LOCALAPPDATA" "DotStar") "dotstar.exe"
 (New-Object System.Net.WebClient).DownloadFile($url, $destination)
+
+Write-Host Refreshing packages...
+Start-Process "$destination refresh" -NoNewWindow -Wait
+
+Write-Host Installing DotStar...
+Start-Process "$destination -i dotstar" -NoNewWindow -Wait

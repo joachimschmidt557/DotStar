6746e760504d2a8d998ae76cd29e4127ce56a54a
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-11-07 11:31:55 +0800

    WIP

--- Get.ps1
+++ Get.ps1
@@ -1,5 +1,3 @@
 $url = "https://github.com/joachimschmidt557/DotStar/releases/download/v0.1.1-alpha/dotstar.exe"
-$file = Join-Path "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)" "dotstar.exe"
-$destination = Join-Path "$Env:LOCALAPPDATA" "DotStar"
-(New-Object System.Net.WebClient).DownloadFile($url, $file)
-Move-Item $file $destination -Force
+$destination = Join-Path "$Env:LOCALAPPDATA" "DotStar" "dotstar.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $destination)

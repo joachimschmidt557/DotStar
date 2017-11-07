6367cb0c7aca9c0175154fd26f6882b45a5e028a
joachimschmidt557 <joachim.schmidt557@outlook.com>
2017-11-07 11:38:52 +0800

    Fix

--- Get.ps1
+++ Get.ps1
@@ -1,3 +1,3 @@
 $url = "https://github.com/joachimschmidt557/DotStar/releases/download/v0.1.1-alpha/dotstar.exe"
-$destination_dir = Join-Path (Join-Path "$Env:LOCALAPPDATA" "DotStar") "dotstar.exe"
+$destination = Join-Path (Join-Path "$Env:LOCALAPPDATA" "DotStar") "dotstar.exe"
 (New-Object System.Net.WebClient).DownloadFile($url, $destination)
